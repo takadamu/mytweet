6b0fa28a0a0a957c68e32b165db8bdf50bbf48af
takadamu <0rg68v3m326626f@gmail.com>
2020-04-15 18:07:19 +0900

    ユーザー別投稿一覧遷移

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_042045) do
+ActiveRecord::Schema.define(version: 2020_04_15_080431) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.text "image"
     t.datetime "created_at", null: false
