583e61fd6b74201376dbe488cf21577c29175232
takadamu <0rg68v3m326626f@gmail.com>
2020-04-18 01:34:36 +0900

    commentControllerメソッド記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+
+def create
+  Comment.create(comment_params)
+end
+
+priveta
+def comment_params
+  params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+end
+
 end
