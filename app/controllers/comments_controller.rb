082e0edeb7be969a3d3528103047e8af688ffd21
takadamu <0rg68v3m326626f@gmail.com>
2020-04-18 13:07:11 +0900

    commentView

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
 class CommentsController < ApplicationController
 
-def create
-  comment = Comment.create(comment_params)
-  redirect_to "/tweets/#{comment.tweet.id}"
-end
+  def create
+    comment = Comment.create(comment_params)
+    redirect_to "/tweets/#{comment.tweet.id}"
+  end
 
-priveta
-def comment_params
-  params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
-end
+  priveta
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+  end
 
 end
