57e10a4673cccc09f7b30024de78aee93c5e2630
takadamu <0rg68v3m326626f@gmail.com>
2020-04-18 12:09:34 +0900

    commentController編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,8 @@
 class CommentsController < ApplicationController
 
 def create
-  Comment.create(comment_params)
+  comment = Comment.create(comment_params)
+  redirect_to "/tweets/#{comment.tweet.id}"
 end
 
 priveta
