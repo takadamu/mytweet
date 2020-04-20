d9745060623883bef62a97ca549684361114e7ef
takadamu <0rg68v3m326626f@gmail.com>
2020-04-20 19:00:52 +0900

    tweetモデルにsearchアクション定義

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,19 @@ class Tweet < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
   has_many :comments
+
+  def self.search(search)
+    return Tweet.all unless search
+    Tweet.where('text LIKE(?)', "%#{search}%")
+  end
+
+  # 上の書き方が分かりにくい場合は下記
+  # def self.search(search)
+  #   if search
+  #     Tweet.where('text LIKR(?)', "%#{search}")
+  #   else
+  #     Tweet.all
+  #   end
+  # end
+
 end
