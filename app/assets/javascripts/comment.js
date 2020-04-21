d4c5ae5167818f8515bfee59a1e79903e492b513
takadamu <0rg68v3m326626f@gmail.com>
2020-04-21 23:19:02 +0900

    comment.js記述

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,4 +1,14 @@
 $(function(){
+  function buildHTML(comment){
+    var html = `<p>
+                  <strong>
+                    <a href=/users/${comment.user_id}>${comment.user_name}</a>
+                    :
+                  </strong>
+                  ${comment.text}
+                </p>`
+    return html;
+  }
   $('#new_comment').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +20,12 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.comments').append(html);
+      $('.textbox').val('');
+      $('.form__submit').prop('disabled', false);
     })
   })
 })
